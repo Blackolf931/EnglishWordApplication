e8aee29a1f15eef663c749d2821df5fc49c16cf8
BlackOlf931 <denis.kagukin@gmail.com>
2022-12-22 13:38:49 +0300

    update word components

--- AndroidApp/core/interfaces/Word.ts
+++ AndroidApp/core/interfaces/Word.ts
@@ -1,13 +1,5 @@
 export interface WordsData {
     englishWord: string,
     translateWord: string,
-<<<<<<< HEAD
     translatedWords: string[],
-=======
-    firstTranslatedWord: string,
-    secondTranslatedWord:string,
-    thirdTranslatedWord : string,
-    fourthTranslatedWord : string
-    translatedWords: string[]
->>>>>>> 2b26939d7529b2af2c4af85597c031063e6fb7a5
 }
