3996973746056a8c16fb450409ce3912df01b9ea
Denis Kagykin <denis.kagukin@gmail.com>
2023-03-05 20:58:42 +0300

    configure bll layer

---
+++ Bll/Models/EnglishTranslatedWordModel.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+
+namespace Bll.Models
+{
+    public class EnglishTranslatedWordModel
+    {
+        public Guid Id { get; set; }
+        public Guid EnglishWordId { get; set; }
+        public Guid TranslatedWordId { get; set; }
+        public bool IsRightTranslated { get; set; }
+        public virtual EnglishWordEntity EnglishWord { get; set; } = null!;
+        public virtual TranslatedWordEntity TranslatedWord { get; set; } = null!;
+    }
+}
---
+++ Bll/Models/TranslatedWordModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bll.Models
+{
+    public class TranslatedWordModel
+    {
+        public Guid Id { get; set; }
+        public string TranslateWord { get; set; } = null!;
+    }
+}
